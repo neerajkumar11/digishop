b9fb8b38abeb66cc0cb045440753e94dd888347c
Neeraj Kumar <nrjkumar1@outlook.com>
2020-10-22 11:03:40 +0530

    Product Detail frontend completed and implemented react router

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {Card} from 'react-bootstrap';
 import Rating from './Rating';
 
@@ -6,15 +7,15 @@ const Product = (props) => {
     return (
         <>
           <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${props.product._id}`}>
+            <Link to={`/product/${props.product._id}`}>
               <Card.Img src={props.product.image} variant='top' />
-            </a>
+            </Link>
             <Card.Body>
-              <a href={`/product/${props.product._id}`}>
+              <Link to={`/product/${props.product._id}`}>
                 <Card.Title as='div'>
                   <strong>{props.product.name}</strong>
                 </Card.Title>
-              </a>
+              </Link>
               <Card.Text as='div'>
                 <div className='my-3'>
                   <Rating rating={props.product.rating} ratingNum={props.product.numReviews}/>
