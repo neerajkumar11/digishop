2fa5a65d9b616fc66247a1ccf983f13aa6544edd
Neeraj Kumar <nrjkumar1@outlook.com>
2020-12-27 19:38:30 +0530

    Deployement preparation

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
@@ -14,13 +15,22 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running....');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes );
 app.use('/api/orders', orderRoutes );
+
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 app.use(notFound);
 
